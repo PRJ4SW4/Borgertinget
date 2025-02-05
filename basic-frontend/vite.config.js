bc2a347f6ee5792411e5d30b24fea44da83845ce
Shaaux <borgstil@gmail.com>
2025-02-05 23:36:52 +0100

    linting fixed now fixing proper tests for frontend

--- basic-frontend/vite.config.js
+++ basic-frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true, // Enables global `test`, `expect` functions
+    environment: "jsdom", // Simulates browser-like environment
+    setupFiles: "./src/test/setup.js", // Optional: Jest DOM setup
+  },
+});
