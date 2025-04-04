ed138c548fd0b0b10eaee9b68dcfa5a94a39bdeb
Nowack02 <202308256@post.au.dk>
2025-04-04 11:52:29 +0200

    Add hardcoded development user and update migration files for testing

--- backend/Data/DataContext.cs
+++ backend/Data/DataContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using backend.Models;
+using BCrypt.Net;
 
 namespace backend.Data;
 
@@ -10,4 +11,25 @@ public class DataContext : DbContext
     }
 
     public DbSet<User> Users { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Hardcoded user for testing purposes
+        var id = 1;
+        var email = "dev@testing.com";
+        var username = "dev";
+        var password = BCrypt.Net.BCrypt.HashPassword("Dev12345");
+
+        modelBuilder.Entity<User>().HasData(new User
+        {
+            Id = id,
+            Email = email,
+            UserName = username,
+            PasswordHash = password,
+            IsVerified = true,
+            VerificationToken = null
+        });
+    }
 }
