048001b52c5f5842b7a43ac3919be770483d753c
Lasse Fink <lassefik3@gmail.com>
2025-04-08 10:18:36 +0200

    feat: LandingPage - færdiggjort styling - mangler at implementerer rigtige knapper - mangler en navbar

--- frontend/src/components/LandingPage/First/FirstSection.tsx
+++ frontend/src/components/LandingPage/First/FirstSection.tsx
@@ -1,9 +1,9 @@
 // src/components/LandingPage/First/FirstSection.tsx
 import React from "react";
 import styles from "./FirstSection.module.css";
-import firstSectionImage from "../../../assets/first_section_image.png"; // Juster stien til dit eksempelbillede
-// TODO: Log ind knap component her >>>> import Button from "../../Common/Button";
-import Button from "../../common/Button";
+import firstSectionImage from "../../../assets/first_section_image.png";
+import Button from "../../common/Button"; // Sørg for at stien er korrekt
+//TODO: Erstat Button med den aktuellle Log in knap
 
 interface FirstSectionProps {
   id?: string;
@@ -13,9 +13,20 @@ const FirstSection: React.FC<FirstSectionProps> = ({ id }) => {
   return (
     <section id={id} className={styles.firstSection}>
       <div className={styles.content}>
-        <h1>For et bedre samfund</h1>
-        <p>Danmarks førende politiske læringsplatform</p>
-        <Button onClick={() => console.log("Log på klikket")}>Log på</Button>
+        {/* Tekstdel */}
+        <div>
+          {" "}
+          {/* Valgfri wrapper for h1 og p, hvis du vil style dem samlet */}
+          <h1>For et bedre samfund</h1>
+          <p>Danmarks førende politiske læringsplatform</p>
+        </div>
+        {/* Knap med specifik klasse */}
+        <Button
+          className={styles.ctaButton} // Tilføjet klasse
+          onClick={() => console.log("Log på klikket")}
+        >
+          Log på
+        </Button>
       </div>
       <div className={styles.imageContainer}>
         <img
