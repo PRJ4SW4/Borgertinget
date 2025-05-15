6a5b17b1bd290c902c94cc3b7410fd386e3380c8
Magnus Hvidsten <magnus.hvidsten@hotmail.dk>
2025-05-15 09:35:41 +0200

    lille refactoring

--- backend/Services/Politician/IFetchService.cs
+++ backend/Services/Politician/IFetchService.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Mvc; // For IActionResult if detailed results are needed
-using System.Threading.Tasks;
 
-namespace backend.Services.Politician{
-    public interface IFetchService{
+namespace backend.Services.Politician
+{
+    public interface IFetchService
+    {
         Task<(int totalAdded, int totalUpdated, int totalDeleted)> FetchAndUpdateAktorsAsync();
     }
 }
-
